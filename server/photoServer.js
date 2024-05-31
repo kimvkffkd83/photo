59e2fc9a77360c5dbbd7f0d3cabb708064169d6f
kimvkffkd83 <se6651@gmail.com>
2024-05-31 23:18:29 +0900

    Feat: Handling error

--- server/photoServer.js
+++ server/photoServer.js
@@ -8,11 +8,12 @@ const fs =  require("fs");
 
 const app = express();
 const port = 5000;
+const limit = 10000000; //10메가바이트
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ limit: 5000000, extended:false, parameterLimit:5000000}));
+app.use(bodyParser.urlencoded({ limit: limit, extended:false, parameterLimit:limit}));
 // app.use(bodyParser.text({type: '/'}));
-app.use(bodyParser.json({limit: 5000000}));
+app.use(bodyParser.json({limit: limit}));
 //정적 파일 위치 설정
 app.use(express.static(path.join(path.resolve(), 'public','img','uploads')));
 
@@ -40,25 +41,34 @@ const upload = multer({
     }),
     // 파일 크기 제한 설정
     limits : {
-        files:10, fileSize: 5000000
+        files:10, fileSize: limit
     }
-})
+}).single('img')
 
 app.listen(port, ()=>{
     console.log("포토 서버 개시 완료")
 })
 
-app.post('/img/:route', upload.single('img'), (req,res) =>{
-    try {
-        console.log("저장할 위치",req.params.route);
-        console.log('전달받은 파일', req.file);
-        console.log('저장된 파일의 이름', req.file.filename);
+app.post('/img/:route', (req,res) =>{
+    upload(req, res, function (err) {
+        if (err) {
+            console.log(`사진 업로드 중 에러 : ${err}`);
+            res.status(500).send(`사진을 불러올 수 없습니다.`);
+            return;
+        }
 
-        // 파일이 저장된 경로를 클라이언트에게 반환해준다.
-        const IMG_URL = `http://localhost:${port}/${req.params.route}/${req.file.filename}`;
-        console.log(IMG_URL);
-        res.json({ url: IMG_URL });
-    }catch (err){
-        console.log("Error!")
-    }
+        try {
+            console.log("저장할 위치",req.params.route);
+            console.log('전달받은 파일', req.file);
+            console.log('저장된 파일의 이름', req.file.filename);
+
+            // 파일이 저장된 경로를 클라이언트에게 반환해준다.
+            const IMG_URL = `http://localhost:${port}/${req.params.route}/${req.file.filename}`;
+            console.log(IMG_URL);
+            res.json({ url: IMG_URL });
+        }catch (err){
+            console.log("Error!")
+        }
+    })
+
 })
